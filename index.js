8b16ee46b1ca27c461c99973422f00621a0fd207
fatush254 <fgalgalo945@gmail.com>
2024-04-01 10:37:21 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+
+    const input = document.getElementaryById('button');
+
+    function clickAlert(){
+        alert('iwascicked!');
+    }
+
+    input.addEventlistener('click', clickAlert);
 }
+console.log(addingEventListener);
